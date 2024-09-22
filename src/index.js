d2b762646981bce419f07cecfc105c411645ebe5
Tina344 <ndabambitina1@gmail.com>
2024-09-22 15:16:23 +0200

    added api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+
+function searchCity (city) {
+
+    let apiKey = "30682do0b1e5e894ad6f6e5480tf504f";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
+}
+    
+
 function SearchSubmitForm(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
