560931af5c59e5a3ac0462d98ecb6dc4f8970e1c
Tina344 <ndabambitina1@gmail.com>
2024-09-22 16:56:03 +0200

    added more js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ function refreshWeather(response) {
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector("#icon");
+
 
     cityElement.innerHTML = response.data.city;
     timeElement.innerHTML = formatDate(date);
@@ -14,6 +16,12 @@ function refreshWeather(response) {
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     descriptionElement.innerHTML = response.data.condition.description;
     temperatureElement.innerHTML = Math.round(temperature);
+    iconElement.innerHTML = `
+      <img
+        src="${response.data.condition.icon_url}"
+        class="weather-app-icon"
+      />
+    `;
 }
 function formatDate(date) {
   let minutes = date.getMinutes();
